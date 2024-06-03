b5bfd32af2794d6b9ed6057f2c6421aed71e4197
Yaroslav Baramykov <105550736+iamzanuda@users.noreply.github.com>
2024-06-03 16:09:45 +0300

    Настроил админку для отображения Отзывов и Избранного.

--- backend/my_store/settings.py
+++ backend/my_store/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Default primary key field type
+# Default primary key field typ
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
